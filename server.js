c5e9b8a48780848d85d93a37d710f4ad9b52f4c3
adityadhara040505 <103095471+adityadhara040505@users.noreply.github.com>
2025-02-23 01:21:54 +0530

    server is running

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use((req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 50002;
 
 const startServer = () => {
     app.listen(PORT, () => {
